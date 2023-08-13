0eeb28e39f1841f3397f391b1f8f4f4216db9b0d
christian <cristhho@gmail.com>
2023-08-12 22:21:10 -0500

    cdk table DataSource

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 import {OverlayModule} from '@angular/cdk/overlay'
 import {CdkAccordionModule} from '@angular/cdk/accordion'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
@@ -7,6 +7,7 @@ import {DragDropModule} from '@angular/cdk/drag-drop'
 import {DialogModule} from '@angular/cdk/dialog'
 import {ScrollingModule} from '@angular/cdk/scrolling'
 import {CdkTableModule} from '@angular/cdk/table'
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -14,11 +15,11 @@ import { LoginComponent } from './pages/login/login.component'
 import { BtnComponent } from './components/btn/btn.component'
 import { BoardsComponent } from './pages/boards/boards.component'
 import { NavbarComponent } from './components/navbar/navbar.component'
-import { BoardComponent } from './pages/board/board.component';
-import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component';
+import { BoardComponent } from './pages/board/board.component'
+import { TodoDialogComponent } from './components/todo-dialog/todo-dialog.component'
 import { ScrollComponent } from './pages/scroll/scroll.component'
-import { HttpClientModule } from '@angular/common/http';
-import { TableComponent } from './pages/table/table.component';
+import { HttpClientModule } from '@angular/common/http'
+import { TableComponent } from './pages/table/table.component'
 
 @NgModule({
   declarations: [
@@ -42,6 +43,7 @@ import { TableComponent } from './pages/table/table.component';
     HttpClientModule,
     ScrollingModule,
     CdkTableModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/pages/table/table.component.ts
+++ src/app/pages/table/table.component.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http'
 import { Component } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 import { Product } from '@models/product'
+import { ProductDataSource } from './data-source'
+import { debounceTime } from 'rxjs'
 
 @Component({
   selector: 'app-table',
@@ -9,15 +12,27 @@ import { Product } from '@models/product'
 })
 export class TableComponent {
 
-  products: Product[] = []
-  columns = ['id', 'title', 'price', 'cover']
+  products = new ProductDataSource()
+  columns = ['id', 'title', 'price', 'cover', 'actions']
+  search = new FormControl('', { nonNullable: true })
 
   constructor(
     private readonly httClient: HttpClient
   ) {
     this.httClient.get<Product[]>('https://api.escuelajs.co/api/v1/products')
       .subscribe((res) => {
-        this.products = res
+        this.products.init(res)
       })
+
+    this.search.valueChanges
+    .pipe(debounceTime(500)).subscribe((value) => {
+      this.products.find(value)
+    })
+  }
+
+  update(product: Product) {
+    this.products.update(product.id, {
+      price: 100
+    })
   }
 }
